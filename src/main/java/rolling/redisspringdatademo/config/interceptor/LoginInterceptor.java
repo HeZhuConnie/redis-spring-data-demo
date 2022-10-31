13947921b4d39998e35f3937f7e47c7b74d36616
HeZhuConnie <he.zhu@thoughtworks.com>
2022-11-01 00:49:29 +0800

    save user in redis, instead of session. Put user token to http header authorization(manually).

--- src/main/java/rolling/redisspringdatademo/config/interceptor/LoginInterceptor.java
+++ src/main/java/rolling/redisspringdatademo/config/interceptor/LoginInterceptor.java
@@ -1,32 +1,40 @@
 package rolling.redisspringdatademo.config.interceptor;
 
+import cn.hutool.core.bean.BeanUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import rolling.redisspringdatademo.controller.dto.UserDto;
 import rolling.redisspringdatademo.utils.UserHolder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+import static rolling.redisspringdatademo.utils.RedisConstants.LOGIN_USER_KEY;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 1. 获取session
-        HttpSession session = request.getSession();
+        // 1. 获取 token
+        String token = request.getHeader("authorization");
 
-        // 2。获取session中的用户
-        Object user = session.getAttribute("user");
-
+        // 2。获取redis中的用户
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(LOGIN_USER_KEY + token);
         // 3。判断用户是否存在
-        if (user == null) {
+        if (userMap.isEmpty()) {
             // 4。不存在，拦截
             response.setStatus(401);
             return false;
         }
 
         // 5。存在，保存用户信息到ThreadLocal，放行
-        UserHolder.saveUser((UserDto) user);
+        UserDto user = BeanUtil.fillBeanWithMap(userMap, new UserDto(), false);
+        UserHolder.saveUser(user);
 
         return true;
     }
