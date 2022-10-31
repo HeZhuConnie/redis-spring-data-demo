9724673f5c214d8e9f234c29ff130eab235daa34
HeZhuConnie <he.zhu@thoughtworks.com>
2022-11-01 01:15:42 +0800

    Resource can replace Autowired in this situation

--- src/main/java/rolling/redisspringdatademo/config/MvcConfig.java
+++ src/main/java/rolling/redisspringdatademo/config/MvcConfig.java
@@ -1,15 +1,16 @@
 package rolling.redisspringdatademo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import rolling.redisspringdatademo.config.interceptor.LoginInterceptor;
 
+import javax.annotation.Resource;
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-    @Autowired
+    @Resource
     private StringRedisTemplate stringRedisTemplate;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
