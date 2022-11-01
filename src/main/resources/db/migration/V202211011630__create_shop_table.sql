fe47a4acf04527e32ce669572c826b8a1b230fdc
HeZhuConnie <he.zhu@thoughtworks.com>
2022-11-01 16:42:53 +0800

    fix: modify db field name

--- src/main/resources/db/migration/V202211011630__create_shop_table.sql
+++ src/main/resources/db/migration/V202211011630__create_shop_table.sql
@@ -2,7 +2,7 @@ create table t_shop
 (
     id                         VARCHAR(36)      NOT NULL PRIMARY KEY,
     name                       VARCHAR(255),
-    typeId                     NUMERIC,
+    type_id                     NUMERIC,
     images                     VARCHAR(255)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
