d31fbfc7350c374c6876769d2e3b1474d38143cb
HeZhuConnie <he.zhu@thoughtworks.com>
2022-11-09 16:46:29 +0800

    create table tb_voucher_order

---
+++ src/main/resources/db/migration/V202211091630__create_voucher_order_table.sql
@@ -0,0 +1,15 @@
+create table tb_voucher_order
+(
+    id bigint(20)               NOT NULL PRIMARY KEY,
+    user_id bigint(20)          UNSIGNED NOT NULL,
+    voucher_id bigint(20)       UNSIGNED NOT NULL,
+    pay_type tinyint(1)         UNSIGNED NOT NULL DEFAULT '1' COMMENT '支付方式 1:金额支付; 2:支付宝; 3:微信;',
+    status tinyint(1)           UNSIGNED NOT NULL DEFAULT '1' COMMENT '订单状态 1:未支付; 2:已支付; 3:已核销; 4:已取消;',
+    create_time                 timestamp NOT NULL DEFAULT current_timestamp,
+    pay_time                    timestamp NULL DEFAULT NULL,
+    use_time                    timestamp NULL DEFAULT NULL,
+    refund_time                 timestamp NULL DEFAULT NULL,
+    update_time                 timestamp NOT NULL DEFAULT current_timestamp
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE utf8mb4_bin;
