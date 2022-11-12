4d4757e7a2a2c480cce2596974b19c5066609233
HeZhuConnie <he.zhu@thoughtworks.com>
2022-11-12 17:07:22 +0800

    防止线程阻塞之后，锁超时释放，当线程醒了之后，误删了其他线程的锁。

--- src/main/java/rolling/redisspringdatademo/utils/RedisLock.java
+++ src/main/java/rolling/redisspringdatademo/utils/RedisLock.java
@@ -1,5 +1,6 @@
 package rolling.redisspringdatademo.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -15,17 +16,24 @@ public class RedisLock implements ILock{
     }
 
     private static final String KEY_PREFIX="lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程标识
-        long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         Boolean success = redis.opsForValue().setIfAbsent(KEY_PREFIX + lockName, threadId + "", timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success); // 这里避免拆箱会有空指针报错！！ 细节！！
     }
 
     @Override
     public void unlock() {
-        redis.opsForValue().getAndDelete(KEY_PREFIX+lockName);
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+
+        String id = redis.opsForValue().get(KEY_PREFIX+lockName);
+
+        if (id.equals(threadId)) {
+            redis.opsForValue().getAndDelete(KEY_PREFIX+lockName);
+        }
     }
 }
